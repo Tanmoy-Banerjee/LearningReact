2c69b1758f118d7e0ffd32f4362b47585b8c8f4d
Tanmoy-Banerjee <tanmoybanerjeetvf@gmail.com>
2023-12-30 16:30:34 +0530

    Learn about creating project, various import export components usage of jsx etc etc.

---
+++ create-react-comp/create-first-comp/src/App.jsx
@@ -0,0 +1,13 @@
+import KgButton from "./KgButton";
+
+function App() {
+  return <div>
+    <h1>THis is best React course</h1>
+    <h1>
+      <button>Subscribe</button>
+    </h1>
+    <KgButton></KgButton>
+  </div>
+}
+
+export default App;
