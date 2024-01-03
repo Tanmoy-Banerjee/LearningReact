05a4dfa6f95299ecca92834c0546e65f5099f58a
Tanmoy-Banerjee <tanmoybanerjeetvf@gmail.com>
2024-01-03 21:50:41 +0530

    Adding more files

---
+++ Projects/4-calculator-version-1/calculator/src/App.jsx
@@ -0,0 +1,15 @@
+import styles from "./App.module.css";
+
+function App() {
+  
+  return (
+    <div id="calculator">
+      <input id="display" type="text"></input>
+      <div id="buttons-container">
+        <button>c</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
