cbf95dcb66a2d6245e98e221685ee8208ee644e5
Tanmoy-Banerjee <tanmoybanerjeetvf@gmail.com>
2024-01-08 22:08:36 +0530

    learning event handlers

--- 18 Fragments/learning-fragment/src/App.jsx
+++ 18 Fragments/learning-fragment/src/App.jsx
@@ -3,28 +3,36 @@ import FoodItems from "./components/FoodItems";
 import ErrorMessage from "./components/ErrorMessage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Container from "./components/Container";
+import FoodInput from "./components/FoodInput";
 
-
-function App(){
-
+function App() {
   //let foodItems = [];
-   let foodItems = ["sabji","Green vegetables","Roti","Salad","Milk","Ghee"];
-return(
-//or you can add <></>  
+  let foodItems = [
+    "sabji",
+    "Green vegetables",
+    "Roti",
+    "Salad",
+    "Milk",
+    "Ghee",
+  ];
+  return (
+    //or you can add <></>
 
+    <>
+      <Container>
+        <h1 className="food-heading">Healthy Food</h1>
 
-<React.Fragment>
+        <ErrorMessage items={foodItems}></ErrorMessage>
+        <FoodInput></FoodInput>
+        <FoodItems items={foodItems}></FoodItems>
+      </Container>
 
-  <h1 className="food-heading">Healthy Food</h1>
-   
-   <ErrorMessage items={foodItems}></ErrorMessage>
-   <FoodItems items = {foodItems}></FoodItems>
-  
-
-
-</React.Fragment>
-
-);
+      <Container>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, consequatur laudantium aperiam harum dicta animi reprehenderit officia repudiandae architecto adipisci provident eius, nulla voluptatibus commodi. Aliquid laborum eum tempore earum.</p>
+      </Container>
+    </>
+  );
 }
 
 export default App;
--- 18 Fragments/learning-fragment/src/components/Item.jsx
+++ 18 Fragments/learning-fragment/src/components/Item.jsx
@@ -1,9 +1,15 @@
+
 function Item(props){
 
+    function handleBuyButtonClick(event){
+       console.log(event);
+       console.log("Buy Button clicked")
+    }
     return (
 
         <li  class="list-group-item kg-item">
             <span className="kg-span">{props.foodItem}</span>
+            <button className="button btn btn-info" onClick={(event) => handleBuyButtonClick(event)}>Buy</button>
       </li>
     )
 
